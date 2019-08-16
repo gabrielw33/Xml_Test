958f187e5fd9f3b77007c3de6747748199fdee4e
gabrielw333 <gabriel.w33@protonmail.com>
2019-08-16 13:06:06 +0200

    renamed and clean code

--- url-to-json/url-to-json.py
+++ url-to-json/url-to-json.py
@@ -7,11 +7,12 @@ import argparse
 
 parser = argparse.ArgumentParser(description='url')
 parser.add_argument('url', type=str, help="url")
-parser.add_argument('reg', type=str, help="the regular expression must be enclosed in brackets and quotation marks") 
+parser.add_argument('reg', type=str, 
+    help="the regular expression must be enclosed in brackets and quotation marks")
 args = parser.parse_args()
 
 
-if len(sys.argv) < 2 :
+if len(sys.argv) < 2:
     print("Error : no parameter")
 else:    
     url = args.url
@@ -22,15 +23,10 @@ reg = args.reg
 match = re.finditer(reg, responce.text)
 
 for matchNum, match in enumerate(match, start=1):
-  
     key = match.group()
     dictionary[key] = -2
-    
 
-json = json.dumps(dictionary,indent=4)
-f = open("dict2.json","w")
+json = json.dumps(dictionary, indent=4)
+f = open("dict2.json", "w")
 f.write(json)
 f.close()
-
-
-
